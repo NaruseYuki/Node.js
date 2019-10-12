93b0cecefbe4ead4e63de2cf87fe8717b1e1cf80
佑樹 鳴瀬 <wishartshingeki@gmail.com>
2019-10-12 10:38:54 +0900

    chapter2フォルダを作成

---
+++ chapter 2/message.js
@@ -0,0 +1,9 @@
+//1
+let messages = ["A change of envvironment can be a good thing!",
+"You will make it!",
+"Just run with the code!"
+];
+
+//2
+//REPL
+//1行のアロー関数で、メッセージを順にログ出力
---
+++ chapter 2/positiveMessages.js
@@ -0,0 +1,11 @@
+//1
+let messages = ["A change of envvironment can be a good thing!",
+"You will make it!",
+"Just run with the code!"
+];
+
+//2
+//REPL
+//1行のアロー関数で、メッセージを順にログ出力
+
+messages.forEach(message=>console.log(message));
